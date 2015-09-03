e8fd5872ad630d06a0dcdbbb10a0900209240f8d
Nicola Hughes <nicolarhughes@gmail.com>
2015-09-03 15:34:15 +0100

    Renaming variable for clarity

--- spec/integrations/game_over_spec.rb
+++ spec/integrations/game_over_spec.rb
@@ -4,10 +4,10 @@ describe 'Ending the game' do
 
   it 'the game is over when both opponents have made a choice' do
 
-    g = Game.new Player, RPS
-    g.player_1.choice = 'scissors'
-    g.player_2.choice = 'rock'
-    expect(g).to be_over
+    game = Game.new Player, RPS
+    game.player_1.choice = 'scissors'
+    game.player_2.choice = 'rock'
+    expect(game).to be_over
 
   end
 end
